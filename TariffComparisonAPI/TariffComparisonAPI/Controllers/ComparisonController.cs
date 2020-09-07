6a19fdff7b9ea31f40f4ef776e6e4dcf1d70172a
Jcelento <jmilecelento@gmail.com>
2020-09-07 20:59:08 -0300

    Add project initial structure

---
+++ TariffComparisonAPI/TariffComparisonAPI/Controllers/ComparisonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TariffComparisonAPI.Controllers
+{
+    public class ComparisonController : ApiController
+    {
+        public string Get()
+        {
+            return "";
+        }
+    }
+}
