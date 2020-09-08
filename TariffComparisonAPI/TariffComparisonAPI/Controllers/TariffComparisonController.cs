85985215a7f4ee7dae927e458e787a3c75ae3457
Jcelento <jmilecelento@gmail.com>
2020-09-07 23:50:45 -0300

    Updates TariffComparisonController to use the new comparison service

---
+++ TariffComparisonAPI/TariffComparisonAPI/Controllers/TariffComparisonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using TariffComparisonAPI.Models;
+using TariffComparisonAPI.Services;
+
+namespace TariffComparisonAPI.Controllers
+{
+    public class TariffComparisonController : ApiController
+    {
+        private readonly TariffComparisonService tariffComparisonService;
+
+        public TariffComparisonController()
+        {
+            tariffComparisonService = new TariffComparisonService();
+        }
+
+        public List<Product> GetTariffComparison(int consumption)
+        {
+            return tariffComparisonService.GetTariffComparison(consumption);
+        }
+    }
+}
