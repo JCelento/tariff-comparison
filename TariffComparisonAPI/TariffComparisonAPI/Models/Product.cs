d2f456e75de04d9f61f7c70f11bd3d0b157bbf98
Jcelento <jmilecelento@gmail.com>
2020-09-07 23:45:52 -0300

    Improves product abstraction

--- TariffComparisonAPI/TariffComparisonAPI/Models/Product.cs
+++ TariffComparisonAPI/TariffComparisonAPI/Models/Product.cs
@@ -1,11 +1,12 @@
-﻿
-namespace TariffComparisonAPI.Models
+﻿namespace TariffComparisonAPI.Models
 {
     public abstract class Product 
     {
-        public abstract string Name { get; }
+        public abstract string TariffName { get; }
 
         public abstract double CalculateAnnualCost(int consumptionInKWhPerYear);
+
+        public abstract double AnnualCosts { get; }
 
     }
 }
