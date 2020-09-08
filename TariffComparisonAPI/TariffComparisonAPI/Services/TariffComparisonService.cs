ea24a4a8197fe630f5e126d662ce46527b05c78c
Jcelento <jmilecelento@gmail.com>
2020-09-07 23:47:14 -0300

    Adds TariffComparisonService class to services

---
+++ TariffComparisonAPI/TariffComparisonAPI/Services/TariffComparisonService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TariffComparisonAPI.Models;
+
+namespace TariffComparisonAPI.Services
+{
+    public class TariffComparisonService
+    {
+        private List<Product> products;
+
+
+        public TariffComparisonService()
+        {
+            products = new List<Product>();
+        }
+
+
+        public List<Product> GetTariffComparison(int consumptionInkWhPerYear)
+        {
+            products.Add(new BasicEletricTariff(consumptionInkWhPerYear));
+            products.Add(new PackagedTariff(consumptionInkWhPerYear));
+
+            products.OrderBy(product => product.AnnualCosts);
+
+            return products;
+        }
+
+
+    }
+}
