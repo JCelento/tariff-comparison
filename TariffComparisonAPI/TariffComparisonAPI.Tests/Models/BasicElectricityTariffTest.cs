9f00f3d971eeef617f42211bb8e2b5fbf4e1136b
Jcelento <jmilecelento@gmail.com>
2020-09-08 01:00:45 -0300

    Adds models unit tests

--- TariffComparisonAPI/TariffComparisonAPI.Tests/Models/BasicElectricityTariffTest.cs
+++ TariffComparisonAPI/TariffComparisonAPI.Tests/Models/BasicElectricityTariffTest.cs
@@ -1,13 +1,51 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TariffComparisonAPI.Models;
 
 namespace TariffComparisonAPI.Tests.Models
 {
     [TestClass]
     public class BasicElectricityTariffTest
     {
+
         [TestMethod]
-        public void BasicEletricTariffAnnualCosts_WhenConsumptionIs()
+        public void BasicElectricityTariff_WhenGettingTariffName_ShouldBeBasicElectricityTariff()
         {
+            var basicElectricityTariff = new BasicElectricityTariff(2000);
+
+            Assert.AreEqual("Basic electricity tariff", basicElectricityTariff.TariffName);
         }
+
+        [TestMethod]
+        public void BasicElectricityTariff_WhenConsumptionIs0WhYear_AnnualCostsShouldBe60()
+        {
+            var basicElectricityTariff = new BasicElectricityTariff(0);
+
+            Assert.AreEqual(60, basicElectricityTariff.AnnualCosts);
+        }
+
+        [TestMethod]
+        public void BasicElectricityTariff_WhenConsumptionIs3500kWhYear_AnnualCostsShouldBe770()
+        {
+            var basicElectricityTariff = new BasicElectricityTariff(3500);
+
+            Assert.AreEqual(830, basicElectricityTariff.AnnualCosts);
+        }
+
+        [TestMethod]
+        public void BasicElectricityTariff_WhenConsumptionIs4500kWhYear_AnnualCostsShouldBe990()
+        {
+            var basicElectricityTariff = new BasicElectricityTariff(4500);
+
+            Assert.AreEqual(1050, basicElectricityTariff.AnnualCosts);
+        }
+
+        [TestMethod]
+        public void BasicElectricityTariff_WhenConsumptionIs6000kWhYear_AnnualCostsShouldBe1320()
+        {
+            var basicElectricityTariff = new BasicElectricityTariff(6000);
+
+            Assert.AreEqual(1380, basicElectricityTariff.AnnualCosts);
+        }
+
     }
 }
--- TariffComparisonAPI/TariffComparisonAPI.Tests/Models/PackagedTariffTest.cs
+++ TariffComparisonAPI/TariffComparisonAPI.Tests/Models/PackagedTariffTest.cs
@@ -1,13 +1,51 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TariffComparisonAPI.Models;
 
 namespace TariffComparisonAPI.Tests.Models
 {
     [TestClass]
     public class PackagedTariffTest
     {
+
         [TestMethod]
-        public void PackagedTariffAnnualCosts()
+        public void PackagedTariff_WhenGettingTariffName_ShouldBePackagedTariff()
         {
+            var packagedTariff = new PackagedTariff(1000);
+
+            Assert.AreEqual("Packaged tariff", packagedTariff.TariffName);
         }
+
+        [TestMethod]
+        public void PackagedTariff_WhenConsumptionIs0WhYear_AnnualCostsShouldBe800()
+        {
+            var packagedTariff = new PackagedTariff(0);
+
+            Assert.AreEqual(800, packagedTariff.AnnualCosts);
+        }
+
+        [TestMethod]
+        public void PackagedTariff_WhenConsumptionIs3500kWhYear_AnnualCostsShouldBe800()
+        {
+            var packagedTariff = new PackagedTariff(3500);
+
+            Assert.AreEqual(800, packagedTariff.AnnualCosts);
+        }
+
+        [TestMethod]
+        public void PackagedTariff_WhenConsumptionIs4500kWhYear_AnnualCostsShouldBe950()
+        {
+            var packagedTariff = new PackagedTariff(4500);
+
+            Assert.AreEqual(950, packagedTariff.AnnualCosts);
+        }
+
+        [TestMethod]
+        public void PackagedTariff_WhenConsumptionIs6000kWhYear_AnnualCostsShouldBe1400()
+        {
+            var packagedTariff = new PackagedTariff(6000);
+
+            Assert.AreEqual(1400, packagedTariff.AnnualCosts);
+        }
+
     }
 }
